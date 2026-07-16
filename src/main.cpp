3832d59979c54f3eab3e6c466e0c2709f459ba82
Joseph <josephaidenstein@gmail.com>
2026-07-16 12:53:32 -0400

    First test

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
  * Include the Geode headers.
  */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/CCScheduler.hpp>
+#include <Geode/binding/GJBaseGameLayer.hpp>
+#include <Geode/utils/random.hpp>
 
 /**
  * Brings cocos2d and all Geode namespaces to the current scope.
@@ -9,92 +12,55 @@
 using namespace geode::prelude;
 
 /**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
+ * Hooks the global cocos2d scheduler so we can smoothly drift the game's
+ * time scale toward randomized targets while gameplay is active. This
+ * affects every scheduled update in the engine, which is how GD implements
+ * slow-motion/fast-forward effects internally.
  */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
+class $modify(DrunkScheduler, CCScheduler) {
+	struct Fields {
+		float m_currentScale = 1.f;
+		float m_targetScale = 1.f;
+		float m_timer = 0.f;
+	};
+
+	void update(float dt) {
+		auto mod = Mod::get();
+		bool enabled = mod->getSettingValue<bool>("enabled");
+		bool inGameplay = GJBaseGameLayer::get() != nullptr;
+
+		if (enabled && inGameplay) {
+			m_fields->m_timer -= dt;
+			if (m_fields->m_timer <= 0.f) {
+				float intensity = static_cast<float>(mod->getSettingValue<double>("intensity"));
+				m_fields->m_targetScale = 1.f + utils::random::generate<float>(-intensity, intensity);
+
+				float minInterval = static_cast<float>(mod->getSettingValue<double>("min-interval"));
+				float maxInterval = static_cast<float>(mod->getSettingValue<double>("max-interval"));
+				m_fields->m_timer = utils::random::generate<float>(minInterval, maxInterval);
+			}
+
+			// Smoothly ease toward the target speed instead of snapping to it.
+			m_fields->m_currentScale += (m_fields->m_targetScale - m_fields->m_currentScale) * std::min(dt * 1.5f, 1.f);
+			this->setTimeScale(m_fields->m_currentScale);
+		} else if (this->getTimeScale() != 1.f) {
+			m_fields->m_currentScale = 1.f;
+			m_fields->m_targetScale = 1.f;
+			this->setTimeScale(1.f);
 		}
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
-
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
-
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
-
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
-
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
-
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
+		CCScheduler::update(dt);
 	}
+};
 
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Does this do what I think it does?", "yeah it does im preeeety sure")->show();
-	}
-} ;
+/**
+ * Immediately reset the time scale to normal if the user disables the
+ * effect mid-gameplay, rather than waiting for the next drift cycle.
+ */
+$on_mod(Loaded) {
+	listenForSettingChanges("enabled", [](bool value) {
+		if (!value) {
+			CCDirector::sharedDirector()->getScheduler()->setTimeScale(1.f);
+		}
+	});
+}
